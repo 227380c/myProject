5c997672ce9f5c46d121eeab77bd76ab8977c29b
227380c <1374543595@qq.com>
2018-05-21 10:50:07 +0800

    idea 测试8

--- src/Test/MyTest.java
+++ src/Test/MyTest.java
@@ -35,8 +35,10 @@ public class MyTest {
 		System.out.println(b>>>1);
 		System.out.println(b>>>2);
 		System.out.println(b>>>3);
-        System.out.println(b>>>5);
 		System.out.println(b>>>4);
+        System.out.println(b>>>5);
+		System.out.println(b>>>6);
+		System.out.println(b>>>8);
 	}
 }
 
