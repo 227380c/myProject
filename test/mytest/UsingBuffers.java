fcbb3c1d22a813f92a7c1233e5586a12659e504a
227380c <1374543595@qq.com>
2018-06-29 10:32:30 +0800

    4444

--- test/mytest/UsingBuffers.java
+++ test/mytest/UsingBuffers.java
@@ -55,6 +55,7 @@ public class UsingBuffers {
         System.out.println(Pattern.matches("\\d", "\\d9"));//false
         System.out.println(Pattern.matches("\\d", "\\d111"));//false
         System.out.println(Pattern.matches("\\d", "\\d8123"));//false
+        System.out.println(Pattern.matches("\\d", "\\d81243"));//false
     		StringBuilder sbf=new StringBuilder();
     		int i=0;
     		for(i=0;i<43200000;i++){
